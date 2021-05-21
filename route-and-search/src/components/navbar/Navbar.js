ced4d14f69e1881a5ef4b5e7a6409ef98d542115
prajapatilalit <prajapatilalit150992@gmail.com>
2021-05-21 23:18:16 +0530

    create and update route path in navbar

--- route-and-search/src/components/navbar/Navbar.js
+++ route-and-search/src/components/navbar/Navbar.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div className="nav-container">
+        <div className="nav-logo">logo</div>
+        <div className="nav-item">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/addAlbum">Add Album</Link>
+            </li>
+            <li>
+              <Link to="/albums">Albums</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
