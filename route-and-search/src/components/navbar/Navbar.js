a2c29835d1c8d34a29363f33359cb2f908c987ad
prajapatilalit <prajapatilalit150992@gmail.com>
2021-05-22 17:43:54 +0530

    create navbar

--- route-and-search/src/components/navbar/Navbar.js
+++ route-and-search/src/components/navbar/Navbar.js
@@ -4,20 +4,37 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <div className="nav-container">
-        <div className="nav-logo">logo</div>
-        <div className="nav-item">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/addAlbum">Add Album</Link>
-            </li>
-            <li>
-              <Link to="/albums">Albums</Link>
-            </li>
-          </ul>
+      <div className="nav">
+        <div className="nav-container">
+          <Link to="/" className="logo">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgcPZDK8pLbgCwKEZvOSvBnn-VmFjYtCfoIkL57XvsTuX76g0_1YBZ4XjRJ3bGl-e1ZZo&usqp=CAU"
+              alt="logo"
+            />
+            <span className="sp">Music-Gallery</span>
+          </Link>
+          <div class="search-container">
+            <form>
+              <input type="text" placeholder="Search.." name="search" />
+              <button type="submit">
+                <i class="fa fa-search"></i>
+              </button>
+            </form>
+          </div>
+
+          <div className="nav-item">
+            <ul>
+              <Link to="/">
+                <li>Home</li>
+              </Link>
+              <Link to="/addAlbum">
+                <li>AddAlbum</li>
+              </Link>
+              <Link to="/albums">
+                <li>Albums</li>
+              </Link>
+            </ul>
+          </div>
         </div>
       </div>
     </>
