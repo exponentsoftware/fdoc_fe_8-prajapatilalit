bdab976e02a05ae7b79d81eb84aeb8947315285f
prajapatilalit <prajapatilalit150992@gmail.com>
2021-08-24 21:51:09 +0530

    update  search bar component of handle input

--- route-and-search/src/components/Navbar/SearchBar.js
+++ route-and-search/src/components/Navbar/SearchBar.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  state = {
+    searchText: "",
+  };
+
+  inputSearchHandle = (e) => {
+    this.setState({ searchText: e.target.value });
+  };
+
+  onInputSubmit = (e) => {
+    e.preventDefault();
+  };
+
   render() {
     return (
       <div className="search-container">
-        <form>
-          <input type="text" placeholder="Search.." name="search" />
+        <form onSubmit={this.onInputSubmit}>
+          <input
+            type="text"
+            placeholder="Search.."
+            name="search"
+            onChange={this.inputSearchHandle}
+            value={this.state.searchText}
+          />
           <button type="submit">
             <i className="fa fa-search"></i>
           </button>
